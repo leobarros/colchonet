023a07a92e5fbcb89a20c5707714ed4d528167fe
Leonardo Barros <nickolback@gmail.com>
2015-01-31 06:46:47 -0200

    Corrigindo a sintaxe

--- db/migrate/20150131082725_create_users.rb
+++ db/migrate/20150131082725_create_users.rb
@@ -7,7 +7,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :location
       t.text :bio
 
-      t.index :email, unique :true
+      t.index :email, unique: true
 
       t.timestamps null: false
     end
